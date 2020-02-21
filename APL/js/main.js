68f05ce94ba371edab22337acd58b3a85309f23f
Bane <timoman8@hotmail.com>
2020-02-20 20:54:22 -0500

    triggered

--- APL/js/main.js
+++ APL/js/main.js
@@ -2,9 +2,17 @@
     window.addEventListener('load', function() {
         document.querySelectorAll(".enableImageBtn").forEach((el) => {
             el.addEventListener('click', function() {
-                document.querySelector(`.${this.classList[0]}.apl`).classList.toggle('enabled');
-                document.querySelector(`.${this.classList[0]}.apl`).classList.toggle('disabled');
-                this.classList.toggle('active');
+                if(this.classList.contains('trigger')) {
+                    if(confirm("This may trigger you, do you accept the consequences?")) {
+                        document.querySelector(`.${this.classList[0]}.apl`).classList.toggle('enabled');
+                        document.querySelector(`.${this.classList[0]}.apl`).classList.toggle('disabled');
+                        this.classList.toggle('active');
+                    }
+                } else {
+                    document.querySelector(`.${this.classList[0]}.apl`).classList.toggle('enabled');
+                    document.querySelector(`.${this.classList[0]}.apl`).classList.toggle('disabled');
+                    this.classList.toggle('active');
+                }
             });
         });
     });
