5ac69586a012c7c82f369b8e9e2a3be56e5f8d80
Bane <timoman8@hotmail.com>
2020-02-20 20:59:07 -0500

    trigger fix

--- APL/js/main.js
+++ APL/js/main.js
@@ -2,7 +2,7 @@
     window.addEventListener('load', function() {
         document.querySelectorAll(".enableImageBtn").forEach((el) => {
             el.addEventListener('click', function() {
-                if(this.classList.contains('active') && this.classList.contains('trigger')) {
+                if(!this.classList.contains('active') && this.classList.contains('trigger')) {
                     if(confirm("This may trigger you, do you accept the consequences?")) {
                         document.querySelector(`.${this.classList[0]}.apl`).classList.toggle('enabled');
                         document.querySelector(`.${this.classList[0]}.apl`).classList.toggle('disabled');
